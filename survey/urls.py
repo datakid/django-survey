f157a780a67663c25d6b60bca59663013d59b27f
Colin Powell <colin.powell@me.com>
2010-07-25 09:19:50 -0400

    Really moving urls now. Making them actually make sense in a RESTful way.

--- survey/urls.py
+++ survey/urls.py
@@ -16,26 +16,22 @@ urlpatterns = patterns('',
 
     url(r'^$', visible_survey_list, name='surveys-visible'),
     url(r'^editable/$', editable_survey_list, name='surveys-editable'),
+    url(r'^new/$', survey_add,   name='survey-add'),
 
-    url(r'^answers/(?P<survey_slug>[-\w]+)/$',
+    url(r'^(?P<survey_slug>[-\w]+)/answers/$',
         answers_list,    name='survey-results'),
-    url(r'^answers/(?P<survey_slug>[-\w]+)/(?P<key>[a-fA-F0-9]{10,40})/$',
+    url(r'^(?P<survey_slug>[-\w]+)/answers/(?P<key>[a-fA-F0-9]{10,40})/$',
         answers_detail,  name='answers-detail'),
+    url(r'^(?P<survey_slug>[-\w]+)/edit$', survey_edit,   name='survey-edit'),
+    url(r'^(?P<survey_slug>[-\w]+)/update$', survey_update,   name='survey-update'),
+    url(r'^(?P<survey_slug>[-\w]+)/delete$', object_delete, {'object_type': 'survey', }, name='survey-delete'),
+    url(r'^(?P<survey_slug>[-\w]+)/question/add$', question_add,   name='question-add'),
+    url(r'^(?P<survey_slug>[-\w]+)/(?P<question_id>\d+)/question/update$', question_update,   name='question-update'),
+    url(r'^(?P<survey_slug>[-\w]+)/(?P<object_id>\d+)/question/delete$', object_delete, {'object_type': 'question', }, name='question-delete'),
 
-    url(r'^edit/(?P<survey_slug>[-\w]+)/$', survey_edit,   name='survey-edit'),
-    url(r'^add/$', survey_add,   name='survey-add'),
-    url(r'^update/(?P<survey_slug>[-\w]+)/$', survey_update,   name='survey-update'),
-    url(r'^delete/(?P<survey_slug>[-\w]+)/$', object_delete, {'object_type': 'survey', }, name='survey-delete'),
-
-    url(r'^question/add/(?P<survey_slug>[-\w]+)/$', question_add,   name='question-add'),
-    url(r'^question/update/(?P<survey_slug>[-\w]+)/(?P<question_id>\d+)/$', question_update,   name='question-update'),
-    url(r'^question/delete/(?P<survey_slug>[-\w]+)/(?P<object_id>\d+)/$', object_delete, {'object_type': 'question', }, name='question-delete'),
-
-
-    url(r'^choice/add/(?P<question_id>\d+)/$', choice_add,   name='choice-add'),
-    url(r'^choice/update/(?P<question_id>\d+)/(?P<choice_id>\d+)/$', choice_update,   name='choice-update'),
-    url(r'^choice/delete/(?P<survey_slug>[-\w]+)/(?P<object_id>\d+)/$', object_delete, {'object_type': 'choice', }, name='choice-delete'),
-
+    url(r'^(?P<question_id>\d+)/choice/add$', choice_add,   name='choice-add'),
+    url(r'^(?P<question_id>\d+)/choice/(?P<choice_id>\d+)/update$', choice_update,   name='choice-update'),
+    url(r'^(?P<survey_slug>[-\w]+)/choice/(?P<object_id>\d+)/delete$', object_delete, {'object_type': 'choice', }, name='choice-delete'),
     url(r'^delete_image/(?P<model_string>[-\w]+)/(?P<object_id>\d+)/$', delete_image, name='delete-image'),
     url(r'^(?P<survey_slug>[-\w]+)/$', survey_detail,   name='survey-detail'),
 
