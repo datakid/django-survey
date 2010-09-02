30aabe24113bd123bb338c49190270ca420ed238
Colin Powell <colin.powell@me.com>
2010-09-02 11:48:29 -0400

    Added template tag to load a survey by slug, also associated include template.

--- survey/templatetags/survey.py
+++ survey/templatetags/survey.py
@@ -1,4 +1,5 @@
 from django import template
+from survey.models import Survey
 
 register = template.Library()
 
@@ -15,3 +16,33 @@ def can_view_answers(user, survey):
 def order_by(queryset, args):
     args = [x.strip() for x in args.split(',')]
     return queryset.order_by(*args)
+
+class GetSurveyNode(template.Node):
+    '''
+        Lookup a survey via a slug and add it to the page context.
+        Return the context.
+    '''
+    def __init__(self, slug, context_var):
+        self.obj=Survey.objects.get(slug=slug)
+        self.context_var=context_var
+
+    def render(self, context):
+        context[self.context_var]=self.obj
+        return ''
+
+def do_get_survey(parser, token):
+    '''
+        Retrieves a survey by slug
+        {% get_survey <slug> as <template var> %}
+    '''
+    try:
+        bits = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError(_('tag requires exactly two arguments'))
+    if len(bits) != 4:
+        raise template.TemplateSyntaxError(_('tag requires exactly three arguments'))
+    if bits[2] != 'as':
+        raise template.TemplateSyntaxError(_("second argument to tag must be 'as'"))
+    return GetGalleryNode(bits[1], bits[3])
+
+register.tag('get_survey, do_get_survey)
